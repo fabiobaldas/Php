8fcad745aab803f10e5fb5ab93f75eee68b8a3b3
Fabio Baldas <43051994+fabiobaldas@users.noreply.github.com>
2019-09-02 21:42:37 -0300

    Add files via upload

---
+++ exercicio4.php
@@ -0,0 +1,20 @@
+<?php
+
+// 1.Criar um vetor com 20 elementos inteiros. Imprimir o maior e o menor valor ,sem ordenar, 
+// o percentual de números pares e a média dos elementos do vetor. 
+
+for($i;$i<=20;$i++){
+    $a = array("1","2","3","4","7","8","10","15","19","100","0","2","99","20","30","60","80","5","89","91");
+    $maior = 999;
+    $menor = 0;
+    
+    if($a == 0){
+        $cont++;
+    }
+    
+    if($a[$a] > $maior){
+        $a = $valor[$a];
+    }
+}
+
+?>
